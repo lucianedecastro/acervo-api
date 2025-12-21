ed41f03ebb1e1507a7d4c342e09d017a0773fa21
Luciane <luciane.castro@gmail.com>
2025-12-21 20:28:05 -0300

    insercao jacksonconfig

---
+++ src/main/java/br/com/acervodaatletabrasileira/acervoapi/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package br.com.acervodaatletabrasileira.acervoapi.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        // Para evitar timestamps numéricos
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
+    }
+}
+
